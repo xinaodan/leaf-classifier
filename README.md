eb90560673fe8b87f83234ab58b758f2d241da20
aodanx <xinaodan@gmail.com>
2020-01-25 10:49:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Leaf Classifier
+
+This project aims at building a classifier to identify Coniferous and Deciduous leaves with small data set and resources.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='leaf-classifier',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Aodan Xin',
+    author_email='xinaodan@gmail.com',
+    description='Leaf Classifier'
+)
